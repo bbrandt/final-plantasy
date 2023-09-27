dad5604efa46718c134d9045e74b126fec2b5ad3
bbrandt <bbrandt@eventellect.com>
2023-09-26 19:44:39 -0500

    Log info msg with connection string
    * When configuring EF Context

--- src/Infrastructure.Sqlite/Planning/PlanningContextConfiguration.cs
+++ src/Infrastructure.Sqlite/Planning/PlanningContextConfiguration.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using TRS.FinalPlantasy.Application.Abstractions.Options;
 using TRS.FinalPlantasy.Infrastructure.EntityFramework.Planning;
@@ -8,10 +9,12 @@ namespace TRS.FinalPlantasy.Infrastructure.Sqlite.Planning;
 internal class PlanningContextConfiguration : IPlanningContextConfiguration
 {
     private readonly IOptionsSnapshot<PlanningOptions> _options;
+    private readonly ILogger<PlanningContextConfiguration> _logger;
 
-    public PlanningContextConfiguration(IOptionsSnapshot<PlanningOptions> options)
+    public PlanningContextConfiguration(IOptionsSnapshot<PlanningOptions> options, ILogger<PlanningContextConfiguration> logger)
     {
         _options = options;
+        _logger = logger;
     }
 
     public void Configure(DbContextOptionsBuilder optionsBuilder)
@@ -21,6 +24,10 @@ internal class PlanningContextConfiguration : IPlanningContextConfiguration
         if (string.IsNullOrWhiteSpace(connectionString))
         {
             throw new Exception($"Connection string is not defined for {nameof(PlanningOptions)}");
+        }
+        else
+        {
+            _logger.LogInformation("Connecting to {ConnectionString}", connectionString);
         }
 
         optionsBuilder.UseSqlite(connectionString);
